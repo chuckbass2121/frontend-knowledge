c1fbe7e1c2f52e9f24cbb725b42e4942047c799f
Chuck <chuckbass2121@gmail.com>
2020-11-22 14:37:19 +0800

    add js part

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -34,7 +34,7 @@ const sidebar = buildChildren(workPath);
 module.exports = {
   title: "前端知识",
   description:
-    "记录前端重要知识点和遇到的好文章，同时还有前端重要算法知识，但最关键的是包含各大小厂真题。",
+    "主要记录自己对前端知识的梳理和理解。",
   themeConfig: {
     nav: [{ text: "GitHub", link: "https://github.com/chuckbass2121/frontend-knowledge" }],
     sidebar,
--- docs/README.md
+++ docs/README.md
@@ -1,1 +1,2 @@
 # frontend-knowledge
+主要记录自己对前端知识的梳理和理解。
